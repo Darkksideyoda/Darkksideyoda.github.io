a517f2118045cfc5a2fb0ec7240b7749146eb26d
Bahtiyar KARAKOC <53101771+Darkksideyoda@users.noreply.github.com>
2021-06-09 18:13:44 +0300

    Upload Files

---
+++ MeasureTwo/app.js
@@ -0,0 +1,9 @@
+function privData(){
+    document.getElementById("lotH3").style.visibility = "hidden";
+    document.getElementById("loginLottie").style.visibility="hidden";
+    document.getElementById("lotieSub").style.display="block";
+    document.getElementById("lotieSub").loop
+
+
+ 
+  }
