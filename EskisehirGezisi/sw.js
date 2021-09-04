5d424c601e23e5edcc0b2e491e94054e76f55f6e
Bahtiyar KARAKOC <53101771+Darkksideyoda@users.noreply.github.com>
2021-09-04 03:27:46 +0300

    Update sw.js

--- EskisehirGezisi/sw.js
+++ EskisehirGezisi/sw.js
@@ -1,2 +1,17 @@
-self.addEventListener('install', e => console.log('pwa installed.'));
-self.addEventListener('fetch', event => {}); // şimdilik bir şey yapmaya gerek yok
+/*self.addEventListener('install', e => console.log('pwa installed.'));
+self.addEventListener('fetch', event => {}); // şimdilik bir şey yapmaya gerek yok*/
+
+
+
+// service-worker.js
+self.addEventListener('install', function () {
+    console.log('Install!');
+});
+self.addEventListener("activate", event => {
+    console.log('Activate!');
+});
+self.addEventListener('fetch', function (event) {
+    console.log('Fetch!', event.request);
+});
+
+
